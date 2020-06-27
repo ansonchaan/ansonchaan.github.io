48d53cb998cd19d192ebe40927b79b2780938bae
ansonchaan <ansonchan9523@gmail.com>
2020-06-27 19:23:58 +0800

    updates

---
+++ warmpaper/_next/static/JmwZ6uBZa-2yl6oxdiR7o/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST = (function(a){return {"/":[a],"/[lang]":[a],"/index":[a]}}("static\u002Fchunks\u002Fd72a0d05b4be6d6d06fd701d244ace73b7ed64a0.c6430379855bd30bc068.js"));self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
