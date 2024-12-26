91881431a797316d75ebeb6799c8f3b8ca6ad425
ansonchaan <31402506+ansonchaan@users.noreply.github.com>
2024-12-26 17:10:27 +0800

    add hkstp

---
+++ hkstp/precache-manifest.36c4ce4260be97f4d3bf2c484d972750.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "89072722f0c1840a5b59",
+    "url": "./static/css/main.0820d839.chunk.css"
+  },
+  {
+    "revision": "89072722f0c1840a5b59",
+    "url": "./static/js/main.02d64849.chunk.js"
+  },
+  {
+    "revision": "8c97409f0ee389fe75da",
+    "url": "./static/js/runtime~main.d653cc00.js"
+  },
+  {
+    "revision": "ecb702616be50163cf4c",
+    "url": "./static/js/2.dee572ba.chunk.js"
+  },
+  {
+    "revision": "3b901ee9dd5bab5365551610fa49d7a5",
+    "url": "./static/media/mask.3b901ee9.jpg"
+  },
+  {
+    "revision": "ac32ba1aa799155b19520ac283eee52b",
+    "url": "./index.html"
+  }
+];
