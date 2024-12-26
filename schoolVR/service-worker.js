506b769dd7c4d201d357112a042d64f60b936c27
ansonchaan <31402506+ansonchaan@users.noreply.github.com>
2024-12-26 18:21:46 +0800

    updates

--- schoolVR/service-worker.js
+++ schoolVR/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.5748fd9888bb7510ad563f5609d8cc2f.js"
+  "./precache-manifest.0463b531b238fb4127ff15a3f9a5d9e9.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("./index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
